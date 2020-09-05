d7e2491fdae9aa9f22594d6292d8d489de32ca83
diegoarturosilva@hotmail.com <diegoarturosilva@hotmail.com>
2020-09-05 23:39:57 +0200

    make users with roles

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,9 +1,10 @@
 <?php
 
-/** @var \Illuminate\Database\Eloquent\Factory $factory */
+/** @var Factory $factory */
 
 use App\User;
 use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factory;
 use Illuminate\Support\Str;
 
 /*
@@ -18,11 +19,15 @@ use Illuminate\Support\Str;
 */
 
 $factory->define(User::class, function (Faker $faker) {
+    $created_at=$faker->dateTimeBetween('-5 years','now');
+    $last_login=$faker->dateTimeBetween($created_at,'now');
+    $roleIds = \App\Role::select('id')->pluck('id');
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        'remember_token' => Str::random(10),
+        'username' => $faker->userName,
+        'password' => $faker->password,
+        'last_login' => $last_login,
+        'is_active'=>$faker->boolean(60),
+        'role_id' => $faker->randomElement($roleIds),
+        'created_at'=>$created_at
     ];
 });
---
+++ database/seeds/RolesTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Role;
+class RolesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $role = Role::updateOrCreate(['role' => 'manager']);
+        $role = Role::updateOrCreate(['role' => 'agent']);
+
+    }
+}
