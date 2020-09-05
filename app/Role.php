eedf7d405f6d1b420234cb57cb9b24355759b4c5
diegoarturosilva@hotmail.com <diegoarturosilva@hotmail.com>
2020-09-05 23:40:58 +0200

    model Role

---
+++ app/Role.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Moloquent;
+
+class Role extends Moloquent
+{
+    //
+    protected $connection = 'mongodb';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'role'
+    ];
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        '_id',
+    ];
+    /**
+     * @var integer
+     */
+    protected $primaryKey = 'id';
+
+    public static function boot()
+    {
+        parent::boot();
+        static::saving(function($model) {
+            $first_user = self::select('id')->orderBy('id','desc')->first();
+            if(!$first_user){
+                $model->id=1;
+            }
+            else{
+                $model->id=$first_user->id+1;
+            }
+        });
+    }
+}
